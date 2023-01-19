b06e867c1da11ee649ef728b9b3f1b3b8bece77f
naimar1996 <naimar@code.edu.az>
2023-01-19 18:44:06 +0400

    Calculator

---
+++ ConsoleApp35/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("--------");
+double number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("------");
+double number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter symbols(+,-,*,/");
+string operations = Console.ReadLine();
+switch (operations)
+{
+    case "+":
+        int sum;
+        Console.WriteLine(number1 + number2);
+        break;
+    case "-":
+        int con;
+        Console.WriteLine(number1 - number2);
+        break;
+    case "*":
+        int time;
+        Console.WriteLine(number1 * number2);
+        break;
+    case "/":
+        int division;
+        Console.WriteLine(number1 / number2);
+        break;
+    default:
+        Console.WriteLine("Try again");
+        break;
+}
+
+ 
+
+
+
